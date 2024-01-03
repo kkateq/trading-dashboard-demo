0e17f14ec4ae7b658201e0c945cc167be91b5b8d
kkateq <kateryna.musina@gmail.com>
2024-01-03 11:04:18 +0100

    Binance order bok.

---
+++ dashboard/BinanceOrderBookView.swift
@@ -0,0 +1,18 @@
+//
+//  BinanceOrderBookView.swift
+//  dashboard
+//
+//  Created by km on 02/01/2024.
+//
+
+import SwiftUI
+
+struct BinanceOrderBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BinanceOrderBookView()
+}
