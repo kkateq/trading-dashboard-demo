4940c2dd8e6129d8d86ed3d9eee77e2b1e7ca267
kkateq <kateryna.musina@gmail.com>
2024-01-08 16:28:15 +0100

    Removed duplication.

--- dashboard/bybit/BybitPrivateManager.swift
+++ dashboard/bybit/BybitPrivateManager.swift
@@ -43,7 +43,7 @@ struct BybitPositionResponse: Decodable {
 
 struct BybitOrderData: Decodable, Equatable, Identifiable {
     var id = UUID()
-    
+
     var symbol: String
     var orderId: String
     var side: String
@@ -58,14 +58,9 @@ struct BybitOrderResponse: Decodable {
     var data: [BybitOrderData]
 }
 
-class BybitPrivateManager: WebSocketDelegate, ObservableObject {
+class BybitPrivateManager: BybitSocketDelegate, ObservableObject {
+    var bybitSocket: BybitSocketTemplate
     var isSubscribed: Bool = false
-    var isConnected: Bool = false
-
-    private var api_key = "Ht5vwI67zqtShgGd0i"
-    private var api_secret = "SATUwBQDTjhscfFStGa12YsY9TXfqwl49r7Y"
-    var socket: WebSocket!
-
     let didChangePositions = PassthroughSubject<Void, Never>()
     private var cancellablePositions: AnyCancellable?
 
@@ -87,7 +82,9 @@ class BybitPrivateManager: WebSocketDelegate, ObservableObject {
     }
 
     init() {
- 
+        self.bybitSocket = BybitSocketTemplate(true)
+        bybitSocket.delegate = self
+
         self.cancellablePositions = AnyCancellable($dataPositions
             .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .removeDuplicates()
@@ -97,15 +94,6 @@ class BybitPrivateManager: WebSocketDelegate, ObservableObject {
             .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .removeDuplicates()
             .assign(to: \.orders, on: self))
-        
-        if let obj = URL(string: "wss://stream.bybit.com/v5/private") {
-            var request = URLRequest(url: obj)
-            request.timeoutInterval = 15
-            self.socket = WebSocket(request: request)
-            socket.delegate = self
-
-            socket.connect()
-        }
     }
 
     func subscribe(socket: Starscream.WebSocket) {
@@ -115,20 +103,7 @@ class BybitPrivateManager: WebSocketDelegate, ObservableObject {
         let msg2 = "{\"op\": \"subscribe\", \"args\": [ \"order\" ]}"
 
         socket.write(string: msg2)
-    }
-
-    func authenticate() {
-        let expires = Int(Date().addingTimeInterval(10).timeIntervalSince1970 * 1000)
-        let signature = sign(key: api_secret, expires: expires)
-
-        let msg = "{\"op\": \"auth\", \"args\": [\"\(api_key)\", \(expires), \"\(signature)\" ]}"
-        socket.write(string: msg)
-    }
-
-    func sign(key: String, expires: Int) -> String {
-        let inputData = Data("GET/realtime\(expires)".utf8)
-        let hash = try? HMAC(key: key, variant: .sha2(.sha256)).authenticate(Array(inputData))
-        return hash?.toHexString() ?? ""
+        isSubscribed = true
     }
 
     func parseMessage(message: String) {
@@ -158,58 +133,6 @@ class BybitPrivateManager: WebSocketDelegate, ObservableObject {
 
         } catch {
             LogManager.shared.error("error is \(error.localizedDescription)")
-        }
-    }
-
-    func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {
-        switch event {
-        case .connected(let headers):
-            DispatchQueue.main.async {
-                self.isConnected = true
-
-                self.authenticate()
-                self.subscribe(socket: self.socket)
-            }
-            LogManager.shared.info("websocket is connected: \(headers)")
-        case .disconnected(let reason, let code):
-            DispatchQueue.main.async {
-                self.isConnected = false
-            }
-
-            LogManager.shared.info("websocket is disconnected: \(reason) with code: \(code)")
-        case .text(let string):
-
-            DispatchQueue.main.async {
-                self.parseMessage(message: string)
-            }
-
-        case .binary(let data):
-            LogManager.shared.info("Received data: \(data.count)")
-        case .ping:
-            break
-        case .pong:
-            break
-        case .viabilityChanged:
-            break
-        case .reconnectSuggested:
-            break
-        case .cancelled:
-            LogManager.shared.info("Cancelled connection")
-        case .error(let error):
-
-            handleError(error)
-        case .peerClosed:
-            break
-        }
-    }
-
-    func handleError(_ error: Error?) {
-        if let e = error as? WSError {
-            LogManager.shared.error("websocket encountered an error: \(e.message)")
-        } else if let e = error {
-            LogManager.shared.error("websocket encountered an error: \(e.localizedDescription)")
-        } else {
-            LogManager.shared.error("websocket encountered an error")
         }
     }
 }
